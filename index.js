d3c77734abc8c21ffc4543b6f96f503648113663
Diego Lopes Sakata <dihsakata@gmail.com>
2025-09-24 18:48:59 -0300

    feat(p1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const axios = require('axios')
 
 const { 
-  PROTOCOL, 
-  BASE_URL_GEO, 
-  APPID, 
-  Q,
-  LIMIT
+    PROTOCOL, 
+    BASE_URL_GEO, 
+    APPID, 
+    Q,
+    LIMIT,
 } = process.env
 
 const URL = `${PROTOCOL}://${BASE_URL_GEO}?q=${Q}&limit=${LIMIT}&appid=${APPID}`
@@ -14,7 +14,7 @@ const URL = `${PROTOCOL}://${BASE_URL_GEO}?q=${Q}&limit=${LIMIT}&appid=${APPID}`
 //DISPARANDOA REQUISIÇÃO HTTPS
 const promiseResultante = axios.get(URL)
 
-promiseResultante
+const pesquisa = promiseResultante
 .then((resposta) => {
     const list = resposta.data
     return list
@@ -25,8 +25,33 @@ promiseResultante
         console.log(`Cidade: ${pesquisa.name}`)
         console.log(`Latitude: ${pesquisa.lat}`)
         console.log(`Longitude: ${pesquisa.lon}`)
+
+        previsao(pesquisa)
     });
 })
 .catch((erro) => {
     console.log(`Erro: ${erro}`)
 })
+
+async function previsao(pesquisa){
+    try{
+        const { 
+            PROTOCOL, 
+            BASE_URL_WEATHER, 
+            APPID,
+            UNITS,
+            IDIOM: LANG
+        } = process.env
+        
+        const URL2 = `${PROTOCOL}://${BASE_URL_WEATHER}?lat=${pesquisa.lat}&lon=${pesquisa.lon}&units=${UNITS}&lang=${LANG}&appid=${APPID}`
+        
+        const promiseResultante2 = await axios.get(URL2)
+
+        console.log(`Sensacao termica: ${promiseResultante2.data.main.feels_like}º celsius`)
+        console.log(`Descricao: ${promiseResultante2.data.weather[0].description}`)
+    }
+    catch(err){
+        console.log(`Erro: ${err}`)
+    }
+    
+}
