bdf53c0eed615f79b4b2bf846ea07feeef39e1ef
Diego Lopes Sakata <dihsakata@gmail.com>
2025-09-24 17:38:13 -0300

    fix: conserta console.log da linha 27

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const axios = require('axios')
 
 const { 
   PROTOCOL, 
-  BASE_URL, 
+  BASE_URL_GEO, 
   APPID, 
   Q,
   LIMIT
 } = process.env
 
-const URL = `${PROTOCOL}://${BASE_URL}?q=${Q}&limit=${LIMIT}&appid=${APPID}`
+const URL = `${PROTOCOL}://${BASE_URL_GEO}?q=${Q}&limit=${LIMIT}&appid=${APPID}`
 
 //DISPARANDOA REQUISIÇÃO HTTPS
 const promiseResultante = axios.get(URL)
@@ -17,7 +17,6 @@ const promiseResultante = axios.get(URL)
 promiseResultante
 .then((resposta) => {
     const list = resposta.data
-    console.log(list)
     return list
 })
 .then((resposta) => {
@@ -25,7 +24,7 @@ promiseResultante
     resposta.forEach(pesquisa => {
         console.log(`Cidade: ${pesquisa.name}`)
         console.log(`Latitude: ${pesquisa.lat}`)
-        console.log(`Latitude: ${pesquisa.lon}`)
+        console.log(`Longitude: ${pesquisa.lon}`)
     });
 })
 .catch((erro) => {
